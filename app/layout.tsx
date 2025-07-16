e93620d1988bda92125e11c65469c09af3269b0a
jacky-chan3 <jacky.chan3@ha.org.hk>
2025-07-16 15:41:00 +0800

    fix: update SmoothCursor springConfig parameters for improved performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,7 +101,7 @@ export default async function RootLayout({
 					>
 						{children}
 						<Toaster />
-						<SmoothCursor springConfig={{ damping: 100, stiffness: 1000, mass: 0.3, restDelta: 0.0001 }} />
+						<SmoothCursor springConfig={{ damping: 100, stiffness: 2000, mass: 0.1, restDelta: 0.0001 }} />
 						<DockDemo />
 					</Layout>
 				</ThemeProvider>
