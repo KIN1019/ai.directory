511bee075d9c84b2c8c6688116c2be3510d8b1f5
jacky-chan3 <jacky.chan3@ha.org.hk>
2025-07-23 16:52:55 +0800

    feat: add setup guide for whitelisting tools and update metadata for best practices

--- content/bestPractices/_meta.js
+++ content/bestPractices/_meta.js
@@ -1,3 +1,4 @@
 export default {
-  "prevent-context-poisoning": "Prevent Context Poisoning"
+  "prevent-context-poisoning": "Prevent Context Poisoning",
+  "setup-whitelist-for-tools": "Setup Whitelist for Tools",
 }
